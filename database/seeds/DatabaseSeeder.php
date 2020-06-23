a9fbb62c50c4e8242276aa5fbe14d0506fa6316f
andreM-coder <andre.moura108@hotmail.com>
2020-06-23 23:50:43 +0100

    final commit

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,5 +13,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(UserSeeder::class);
         $this->call(PictureSeeder::class);
+        $this->call(CategorySeeder::class);
+        $this->call(ProductsSeeder::class);
     }
 }
